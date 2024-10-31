4730c25a22ea605d7204d5e2d4bc3887caa97c93
COSME, HAROLD D. <haroldcosme27@gmail.com>
2024-10-31 11:34:01 +0800

    Add files via upload

---
+++ colorPreference.js
@@ -0,0 +1,8 @@
+colors = []
+colors.push(prompt("Enter your first favorite color: "))
+console.log(colors)
+colors.push(prompt("Enter your second favorite color: "))
+console.log(colors)
+colors.push(prompt("Enter your third favorite color: "))
+console.log(colors)
+ 
