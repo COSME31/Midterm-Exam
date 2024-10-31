16f57933277e947461309ce8bbd90dd615f18995
COSME, HAROLD D. <haroldcosme27@gmail.com>
2024-10-31 11:35:36 +0800

    Add files via upload

--- userProfile.js
+++ userProfile.js
@@ -1,11 +1,7 @@
-let fullName = prompt("Enter your full name (first and last name):");
-let age = prompt("Enter your age:");
-let favoriteNumber = prompt("Enter your favorite number:");
-let favoriteColor = prompt("Enter your favorite color:");
-
-console.log("Full Name:", fullName);
-console.log("Age:", age);
-console.log("Favorite Number:", favoriteNumber);
-console.log("Favorite Color:", favoriteColor);
- 
- 
+userProfile = {
+  "fullName": "Harold Cosme",
+  "age": 24,
+  "favoriteNumber": 01,
+  "favoriteColors": ["blue", "black", "white"]
+}
+console.log(userProfile)
